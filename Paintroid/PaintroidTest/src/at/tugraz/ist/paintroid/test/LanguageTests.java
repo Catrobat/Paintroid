abf907138eb14520425d666252fa1343d994d276
Sercan Akpolat <sercan.akpolat@gmail.com>
2011-02-16 15:32:22 +0100

    added new TestFile

---
+++ Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/LanguageTests.java
@@ -0,0 +1,11 @@
+package at.tugraz.ist.paintroid.test;
+
+import at.tugraz.ist.paintroid.MainActivity;
+
+import com.jayway.android.robotium.solo.Solo;
+
+
+public class LanguageTests {
+	private Solo solo;
+	private MainActivity mainActivity;
+}
