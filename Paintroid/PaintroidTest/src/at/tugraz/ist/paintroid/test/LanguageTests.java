6cfdf499f6db063c1de6cdc3eba25d249f3b1db0
Sercan Akpolat <sercan.akpolat@gmail.com>
2011-02-16 16:39:54 +0100

    added testcase but don't know how to update configuration

--- Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/LanguageTests.java
+++ Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/LanguageTests.java
@@ -1,11 +1,51 @@
 package at.tugraz.ist.paintroid.test;
 
+import java.util.Locale;
+
+import android.content.res.Configuration;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import at.tugraz.ist.paintroid.MainActivity;
 
 import com.jayway.android.robotium.solo.Solo;
 
 
-public class LanguageTests {
+public class LanguageTests extends ActivityInstrumentationTestCase2<MainActivity>{
+	
+	public LanguageTests() {
+		super("at.tugraz.ist.paintroid", MainActivity.class);
+	}
+
+	public void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+		
+		
 	private Solo solo;
 	private MainActivity mainActivity;
+	
+	public void testEnglish(){
+		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		solo.clickOnImageButton(7);
+		if(Locale.getDefault().getDisplayLanguage().equals("Deutsch")){
+			solo.clickOnButton("Abbrechen");
+		}
+
+		String languageToLoad  = "en";
+		Locale locale = new Locale(languageToLoad);
+		Locale.setDefault(locale);
+		Configuration config = new Configuration();
+		config.locale = locale;
+		//wie updaten???? -.-"
+		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+		
+		solo.clickOnImageButton(7);
+		solo.clickOnButton("Cancel");
+
+
+	}
+	
+	
 }
