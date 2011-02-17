66661d7272304217b5cf997dcc935ec81ee58425
Sercan Akpolat <sercan.akpolat@gmail.com>
2011-02-17 15:21:26 +0100

    Solved Problem with testcases, now French, German and English are Tested

--- Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/LanguageTests.java
+++ Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/LanguageTests.java
@@ -25,27 +25,130 @@ public class LanguageTests extends ActivityInstrumentationTestCase2<MainActivity
 	private MainActivity mainActivity;
 	
 	public void testEnglish(){
-		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
-		solo.clickOnImageButton(7);
-		if(Locale.getDefault().getDisplayLanguage().equals("Deutsch")){
-			solo.clickOnButton("Abbrechen");
-		}
-
-		String languageToLoad  = "en";
-		Locale locale = new Locale(languageToLoad);
-		Locale.setDefault(locale);
-		Configuration config = new Configuration();
-		config.locale = locale;
-		//wie updaten???? -.-"
-		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		
+		String languageToLoad_before  = "de";
+		Locale locale_before = new Locale(languageToLoad_before);
+		Locale.setDefault(locale_before);
+		
+		Configuration config_before = new Configuration();
+		config_before.locale = locale_before;
+		
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		mainActivity.getBaseContext().getResources().updateConfiguration(config_before, mainActivity.getBaseContext().getResources().getDisplayMetrics());
 		
 		solo = new Solo(getInstrumentation(), getActivity());
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		
+		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		
+		assertEquals("Deutsch", Locale.getDefault().getDisplayLanguage());
+		
+		solo.clickOnImageButton(7);
+		solo.clickOnButton("Abbrechen");
+
+		String languageToLoad_after  = "en";
+		Locale locale_after = new Locale(languageToLoad_after);
+		Locale.setDefault(locale_after);
+		
+		Configuration config_after = new Configuration();
+		config_after.locale = locale_after;
+		
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		mainActivity.getBaseContext().getResources().updateConfiguration(config_after, mainActivity.getBaseContext().getResources().getDisplayMetrics());
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		
+		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		
+		assertEquals("English", Locale.getDefault().getDisplayLanguage());
 		
 		solo.clickOnImageButton(7);
 		solo.clickOnButton("Cancel");
 
-
 	}
 	
+	public void testFrench(){
+		String languageToLoad_before  = "de";
+		Locale locale_before = new Locale(languageToLoad_before);
+		Locale.setDefault(locale_before);
+		
+		Configuration config_before = new Configuration();
+		config_before.locale = locale_before;
+		
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		mainActivity.getBaseContext().getResources().updateConfiguration(config_before, mainActivity.getBaseContext().getResources().getDisplayMetrics());
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		
+		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		
+		assertEquals("Deutsch", Locale.getDefault().getDisplayLanguage());
+		
+		solo.clickOnImageButton(7);
+		solo.clickOnButton("Abbrechen");
+
+		String languageToLoad_after  = "fr";
+		Locale locale_after = new Locale(languageToLoad_after);
+		Locale.setDefault(locale_after);
+		
+		Configuration config_after = new Configuration();
+		config_after.locale = locale_after;
+		
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		mainActivity.getBaseContext().getResources().updateConfiguration(config_after, mainActivity.getBaseContext().getResources().getDisplayMetrics());
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		
+		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		
+		assertEquals("français", Locale.getDefault().getDisplayLanguage());
+		
+		solo.clickOnImageButton(7);
+		solo.clickOnButton("Annuler");
+	}
+	
+	public void testGerman(){
+		String languageToLoad_before  = "fr";
+		Locale locale_before = new Locale(languageToLoad_before);
+		Locale.setDefault(locale_before);
+		
+		Configuration config_before = new Configuration();
+		config_before.locale = locale_before;
+		
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		mainActivity.getBaseContext().getResources().updateConfiguration(config_before, mainActivity.getBaseContext().getResources().getDisplayMetrics());
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		
+		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		
+		assertEquals("français", Locale.getDefault().getDisplayLanguage());
+		
+		solo.clickOnImageButton(7);
+		solo.clickOnButton("Annuler");
+
+		String languageToLoad_after  = "de";
+		Locale locale_after = new Locale(languageToLoad_after);
+		Locale.setDefault(locale_after);
+		
+		Configuration config_after = new Configuration();
+		config_after.locale = locale_after;
+		
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		mainActivity.getBaseContext().getResources().updateConfiguration(config_after, mainActivity.getBaseContext().getResources().getDisplayMetrics());
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+		mainActivity = (MainActivity) solo.getCurrentActivity();
+		
+		Log.d("PaintroidTest","Current language: " + Locale.getDefault().getDisplayLanguage());
+		
+		assertEquals("Deutsch", Locale.getDefault().getDisplayLanguage());
+		solo.clickOnImageButton(7);
+		solo.clickOnButton("Abbrechen");
+	}
 	
 }
