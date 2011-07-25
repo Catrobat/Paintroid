90922335b85e129dd87dcc5b304880cd66502728
Maximilian Fellner <max.fellner@gmail.com>
2011-07-25 19:08:48 +0200

    Now using a shadow activity.

--- Paintroid/Paintroid/test/at/tugraz/ist/paintroid/robolectrictest/HelloTest.java
+++ Paintroid/Paintroid/test/at/tugraz/ist/paintroid/robolectrictest/HelloTest.java
@@ -1,20 +1,25 @@
 package at.tugraz.ist.paintroid.robolectrictest;
 
-import at.tugraz.ist.paintroid.MainActivity;
-import at.tugraz.ist.paintroid.R;
-import com.xtremelabs.robolectric.RobolectricTestRunner;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import at.tugraz.ist.paintroid.MainActivity;
+import at.tugraz.ist.paintroid.R;
+
+import com.xtremelabs.robolectric.Robolectric;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import com.xtremelabs.robolectric.shadows.ShadowActivity;
 
 @RunWith(RobolectricTestRunner.class)
 public class HelloTest {
 
-    @Test
-    public void appNameIsPaintroid() throws Exception {
-        String hello = new MainActivity().getResources().getString(R.string.app_name);
-        assertThat(hello, equalTo("Paintroid"));
-    }
+	@Test
+	public void appNameIsPaintroid() throws Exception {
+		ShadowActivity shadowMainActivity = Robolectric.shadowOf(new MainActivity());
+		String hello = shadowMainActivity.getResources().getString(R.string.app_name);
+		assertThat(hello, equalTo("Paintroid"));
+	}
 }
