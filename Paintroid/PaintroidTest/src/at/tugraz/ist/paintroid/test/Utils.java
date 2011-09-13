1495edab454c1400009d0fa166a8b26d393b9dfa
Maximilian Fellner <max.fellner@gmail.com>
2011-09-13 16:10:57 +0200

    Added tests.

--- Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/Utils.java
+++ Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/Utils.java
@@ -97,4 +97,20 @@ public class Utils {
 		solo.clickOnButton(newColorButton);
 		solo.waitForDialogToClose(500);
 	}
+
+	/**
+	 * @param solo
+	 *            Robotium Solo
+	 * @param viewClassName
+	 *            Concrete Class of the View
+	 * @return true if such a View is visible, false otherwise
+	 */
+	public static boolean viewIsVisible(Solo solo, Class<? extends View> viewClass) {
+		ArrayList<View> visibleViews = solo.getViews();
+		for (int i = 0; i < visibleViews.size(); i++) {
+			if (visibleViews.get(i).getClass().getName().equals(viewClass.getName()))
+				return true;
+		}
+		return false;
+	}
 }
