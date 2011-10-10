2fd23f5f78ddfef68a6f74f3f4c8ef1c620ca665
Maximilian Fellner <max.fellner@gmail.com>
2011-10-10 16:10:00 +0200

    Switched to Affero GPL 3.

--- Paintroid/Paintroid/src/at/tugraz/ist/paintroid/helper/ToolButton.java
+++ Paintroid/Paintroid/src/at/tugraz/ist/paintroid/helper/ToolButton.java
@@ -1,37 +1,35 @@
-/*    Catroid: An on-device graphical programming language for Android devices
- *    Copyright (C) 2010  Catroid development team
- *    (<http://code.google.com/p/catroid/wiki/Credits>)
+/*
+ *   This file is part of Paintroid, a software part of the Catroid project.
+ *   Copyright (C) 2010  Catroid development team
+ *   <http://code.google.com/p/catroid/wiki/Credits>
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   Paintroid is free software: you can redistribute it and/or modify it
+ *   under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   at your option) any later version.
+ *   
+ *   Paintroid is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *   
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with Paintroid.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package at.tugraz.ist.paintroid.helper;
 
 import at.tugraz.ist.paintroid.MainActivity.ToolType;
 
-
 public class ToolButton {
-  
-  public int drawableId;
-  public int stringId;
-  public ToolType buttonId;
-  
-  ToolButton(int drawableId, int stringId, ToolType buttonId)
-  {
-    this.drawableId = drawableId;
-    this.stringId = stringId;
-    this.buttonId = buttonId;
-  }
-  
+
+	public int drawableId;
+	public int stringId;
+	public ToolType buttonId;
+
+	ToolButton(int drawableId, int stringId, ToolType buttonId) {
+		this.drawableId = drawableId;
+		this.stringId = stringId;
+		this.buttonId = buttonId;
+	}
+
 }
