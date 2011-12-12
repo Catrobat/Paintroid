799f4843d16724e6a3e7808c8b4883f10eba6fbd
Maximilian Fellner <max.fellner@gmail.com>
2011-12-13 00:06:11 +0100

    Added tests for Perspective.

--- Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/junit/ui/PerspectiveTests.java
+++ Paintroid/PaintroidTest/src/at/tugraz/ist/paintroid/test/junit/ui/PerspectiveTests.java
@@ -19,6 +19,85 @@
 
 package at.tugraz.ist.paintroid.test.junit.ui;
 
-public class PerspectiveTests {
+import junit.framework.TestCase;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.view.SurfaceHolder;
+import at.tugraz.ist.paintroid.test.junit.stubs.SurfaceHolderStub;
+import at.tugraz.ist.paintroid.test.utils.PrivateAccess;
+import at.tugraz.ist.paintroid.ui.Perspective;
+import at.tugraz.ist.paintroid.ui.implementation.DrawingSurfacePerspective;
 
+public class PerspectiveTests extends TestCase {
+
+	SurfaceHolderStub surfaceHolderStub;
+	Perspective perspective;
+	float actualCenterX;
+	float actualCenterY;
+
+	@Override
+	public void setUp() {
+		surfaceHolderStub = new SurfaceHolderStub();
+		perspective = new DrawingSurfacePerspective(surfaceHolderStub);
+		actualCenterX = SurfaceHolderStub.WIDTH / 2f;
+		actualCenterY = SurfaceHolderStub.HEIGHT / 2f;
+	}
+
+	public void testShouldInitializeCorrectly() throws SecurityException, IllegalArgumentException,
+			NoSuchFieldException, IllegalAccessException {
+		SurfaceHolder holder = (SurfaceHolder) PrivateAccess.getMemberValue(DrawingSurfacePerspective.class,
+				perspective, "surfaceHolder");
+		assertSame(surfaceHolderStub, holder);
+
+		PointF surfaceCenter = (PointF) PrivateAccess.getMemberValue(DrawingSurfacePerspective.class, perspective,
+				"surfaceCenter");
+		assertEquals(actualCenterX, surfaceCenter.x);
+		assertEquals(actualCenterY, surfaceCenter.y);
+
+		PointF surfaceTranslation = (PointF) PrivateAccess.getMemberValue(DrawingSurfacePerspective.class, perspective,
+				"surfaceTranslation");
+		assertEquals(0f, surfaceTranslation.x);
+		assertEquals(0f, surfaceTranslation.y);
+	}
+
+	public void testShouldScaleCorrectly() {
+		Matrix controlMatrix = new Matrix();
+		Canvas canvas = surfaceHolderStub.getCanvas();
+		assertEquals(controlMatrix, canvas.getMatrix());
+
+		float scale = 1.5f;
+		perspective.scale(scale);
+		controlMatrix.postScale(scale, scale, actualCenterX, actualCenterY);
+		assertEquals(controlMatrix, canvas.getMatrix());
+	}
+
+	public void testShouldNotScaleBelowMinimum() {
+		Matrix controlMatrix = new Matrix();
+		Canvas canvas = surfaceHolderStub.getCanvas();
+		assertEquals(controlMatrix, canvas.getMatrix());
+
+		float minScale = DrawingSurfacePerspective.MIN_SCALE;
+		assertEquals(0.5f, minScale);
+
+		float scale = 0.1f;
+		perspective.scale(scale);
+		controlMatrix.postScale(minScale, minScale, actualCenterX, actualCenterY);
+		assertEquals(controlMatrix, canvas.getMatrix());
+	}
+
+	public void testShouldTranslateCorrectly() {
+		Matrix controlMatrix = new Matrix();
+		Canvas canvas = surfaceHolderStub.getCanvas();
+		assertEquals(controlMatrix, canvas.getMatrix());
+
+		float dx = 10f, dy = 20f;
+		perspective.translate(dx, dy);
+		controlMatrix.postTranslate(dx, dy);
+		assertEquals(controlMatrix, canvas.getMatrix());
+	}
+
+	// public void testShouldScaleAndTranslateCorrectly() {
+	//
+	// }
 }
