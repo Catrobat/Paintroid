ea5982e9286d948bb94141d550464a89c77f710f
Maximilian Fellner <max.fellner@gmail.com>
2012-01-10 14:40:02 +0100

    Use custom implementation of Application class to maintain global app state and to store global variables and constants.

---
+++ Paintroid/Paintroid/src/at/tugraz/ist/paintroid/PaintroidApplication.java
@@ -0,0 +1,12 @@
+package at.tugraz.ist.paintroid;
+
+import android.app.Application;
+
+public class PaintroidApplication extends Application {
+	public static final String TAG = "PAINTROID";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
