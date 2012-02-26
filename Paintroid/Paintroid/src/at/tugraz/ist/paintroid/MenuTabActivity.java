0e3ce8605f3da19ba000c0b216741319f6fc0e92
Maximilian Fellner <max.fellner@gmail.com>
2012-02-26 20:45:32 +0100

    Refactored MenuTabActivity.

--- Paintroid/Paintroid/src/at/tugraz/ist/paintroid/MenuTabActivity.java
+++ Paintroid/Paintroid/src/at/tugraz/ist/paintroid/MenuTabActivity.java
@@ -1,20 +1,27 @@
-/*
- *   This file is part of Paintroid, a software part of the Catroid project.
- *   Copyright (C) 2010  Catroid development team
- *   <http://code.google.com/p/catroid/wiki/Credits>
- *
- *   Paintroid is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU Affero General Public License as published
- *   by the Free Software Foundation, either version 3 of the License, or
- *   at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Affero General Public License for more details.
- *
- *   You should have received a copy of the GNU Affero General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010-2011 The Catroid Team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *  
+ *  Paintroid: An image manipulation application for Android, part of the
+ *  Catroid project and Catroid suite of software.
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://www.catroid.org/catroid_license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package at.tugraz.ist.paintroid;
@@ -28,9 +35,6 @@ import android.widget.TabHost;
 
 public class MenuTabActivity extends TabActivity {
 
-	/**
-	 * Called when the activity is first created
-	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,26 +42,22 @@ public class MenuTabActivity extends TabActivity {
 
 		overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out);
 
-		Resources res = getResources(); // Resource object to get Drawables
-		TabHost tabHost = getTabHost(); // The activity TabHost
-		TabHost.TabSpec spec; // Resusable TabSpec for each tab
-		Intent intent; // Reusable Intent for each tab
+		Resources res = getResources();
+		TabHost tabHost = getTabHost();
+		TabHost.TabSpec tabSpec;
+		Intent intent;
 
-		// Create an Intent to launch an Activity for the tab (to be reused)
 		intent = new Intent().setClass(this, FileActivity.class);
-
-		// Initialize a TabSpec for each tab and add it to the TabHost
-		spec = tabHost.newTabSpec("file")
+		tabSpec = tabHost.newTabSpec("file")
 				.setIndicator(getString(R.string.menu_tab_file), res.getDrawable(R.drawable.ic_tab_file))
 				.setContent(intent);
-		tabHost.addTab(spec);
+		tabHost.addTab(tabSpec);
 
-		// Do the same for the other tabs
 		intent = new Intent().setClass(this, ToolMenuActivity.class);
-		spec = tabHost.newTabSpec("menu")
+		tabSpec = tabHost.newTabSpec("menu")
 				.setIndicator(getString(R.string.menu_tab_tools), res.getDrawable(R.drawable.ic_tab_menu))
 				.setContent(intent);
-		tabHost.addTab(spec);
+		tabHost.addTab(tabSpec);
 
 		tabHost.setCurrentTab(1);
 	}
