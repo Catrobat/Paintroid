5cf5f6565d0cf515c46bcfd83ff2c7afe9aa289c
Maximilian Fellner <max.fellner@gmail.com>
2012-02-26 19:49:33 +0100

    Refactored FileIO.

--- Paintroid/Paintroid/src/at/tugraz/ist/paintroid/FileIO.java
+++ Paintroid/Paintroid/src/at/tugraz/ist/paintroid/FileIO.java
@@ -1,20 +1,27 @@
-/*
- *   This file is part of Paintroid, a software part of the Catroid project.
- *   Copyright (C) 2010  Catroid development team
- *   <http://code.google.com/p/catroid/wiki/Credits>
- *
- *   Paintroid is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU Affero General Public License as published
- *   by the Free Software Foundation, either version 3 of the License, or
- *   at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Affero General Public License for more details.
- *
- *   You should have received a copy of the GNU Affero General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010-2011 The Catroid Team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *  
+ *  Paintroid: An image manipulation application for Android, part of the
+ *  Catroid project and Catroid suite of software.
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://www.catroid.org/catroid_license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package at.tugraz.ist.paintroid;
@@ -37,27 +44,6 @@ public class FileIO {
 	private FileIO() {
 	}
 
-	// /**
-	// * @param context Context in which method is called.
-	// * @param bitmap Bitmap which shall be saved. Can be null (only an empty File is created).
-	// * @param uri Uri of the file to be saved.
-	// * @return Null if an error occured, otherwise the File which was created.
-	// */
-	// public static File saveBitmap(Context context, Bitmap bitmap, Uri uri) {
-	// if (uri != null) {
-	// return saveBitmap(context, bitmap, uri.getPath());
-	// } else {
-	// Log.e(PaintroidApplication.TAG, "CANNOT SAVE BITMAP, URI IS NULL");
-	// return null;
-	// }
-	// }
-
-	/**
-	 * @param context Context in which method is called.
-	 * @param bitmap Bitmap which shall be saved. Can be null (only an empty File is created).
-	 * @param name Name of the file to be saved (without filetype).
-	 * @return Null if an error occured, otherwise the File which was created.
-	 */
 	public static File saveBitmap(Context context, Bitmap bitmap, String name) {
 		final int QUALITY = 100;
 		final String ENDING = ".png";
@@ -69,6 +55,7 @@ public class FileIO {
 		} else {
 			file = createNewEmptyPictureFile(context, name + ENDING);
 		}
+
 		if (file != null) {
 			try {
 				bitmap.compress(FORMAT, QUALITY, new FileOutputStream(file));
@@ -78,6 +65,7 @@ public class FileIO {
 				Log.e(PaintroidApplication.TAG, "ERROR writing " + file, e);
 			}
 		}
+
 		return file;
 	}
 
@@ -90,7 +78,7 @@ public class FileIO {
 	}
 
 	public static String getRealPathFromURI(Context context, Uri imageUri) {
-		String path = "";
+		String path = null;
 		String[] filePathColumn = { MediaStore.Images.Media.DATA };
 		Cursor cursor = context.getContentResolver().query(imageUri, filePathColumn, null, null, null);
 
@@ -106,6 +94,7 @@ public class FileIO {
 				Log.e("PAINTROID", "URI ERROR ", e);
 			}
 		}
+
 		return path;
 	}
 }
