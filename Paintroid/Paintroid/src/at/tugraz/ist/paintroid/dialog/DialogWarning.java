69e611bf2ff2035b9be1e4828fb327f704dfd51a
Maximilian Fellner <max.fellner@gmail.com>
2012-03-09 16:29:24 +0100

    Set version number to 0.6.0beta. Inserted new license text.

--- Paintroid/Paintroid/src/at/tugraz/ist/paintroid/dialog/DialogWarning.java
+++ Paintroid/Paintroid/src/at/tugraz/ist/paintroid/dialog/DialogWarning.java
@@ -23,6 +23,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package at.tugraz.ist.paintroid.dialog;
 
 import android.app.Dialog;
