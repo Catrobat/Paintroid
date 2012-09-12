456aebece03b854ecf9dff4da6c23734a09eb6df
Gerald Musser <gerald@musser.ws>
2012-09-12 02:25:43 +0200

    license text changes

--- Paintroid/Paintroid/src/org/catrobat/paintroid/dialog/colorpicker/HsvAlphaSelectorView.java
+++ Paintroid/Paintroid/src/org/catrobat/paintroid/dialog/colorpicker/HsvAlphaSelectorView.java
@@ -19,7 +19,8 @@
  *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ */
+/**
  *    This file incorporates work covered by the following copyright and  
  *    permission notice: 
  *    
