da2699f47851502cd0431699268d7c6fe97d25e1
Stefan Galler <galler.stefan@gmail.com>
2012-11-20 21:58:11 +0100

    resized canvas to fit completely on the screen when starting. set actionbar size to 50dip. somewhere 12dip are hiding

--- Paintroid/src/org/catrobat/paintroid/MenuFileActivity.java
+++ Paintroid/src/org/catrobat/paintroid/MenuFileActivity.java
@@ -55,7 +55,8 @@ public abstract class MenuFileActivity extends SherlockActivity {
 	protected static final int REQ_TAKE_PICTURE = 4;
 	protected static final int REQ_TOOLS_DIALOG = 5;
 
-	protected static final float ACTION_BAR_HEIGHT = 60.0f;
+	// 50dip in style.xml but need 62 here. must be a 12dip padding somewhere.
+	protected static final float ACTION_BAR_HEIGHT = 62.0f;
 
 	public static final String RET_ACTION = "RET_ACTION";
 	public static final String RET_URI = "RET_URI";
