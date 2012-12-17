374caa427ec00644cc1107ce5fc86ca6254f66e6
markusschmid <markusschmid@cp21-70.tu-graz.ac.at>
2012-12-17 11:17:51 +0100

    testing border -10

--- PaintroidTest/src/org/catrobat/paintroid/test/integration/ColorDialogIntegrationTest.java
+++ PaintroidTest/src/org/catrobat/paintroid/test/integration/ColorDialogIntegrationTest.java
@@ -174,7 +174,7 @@ public class ColorDialogIntegrationTest extends BaseIntegrationTestClass {
 		assertTrue("Waiting for DrawingSurface", mSolo.waitForView(DrawingSurfaceImplementation.class, 1, TIMEOUT));
 		mSolo.clickOnView(mMenuBottomParameter2);
 		assertTrue("Waiting for DrawingSurface", mSolo.waitForText(mSolo.getString(R.string.ok), 1, TIMEOUT * 2));
-		mSolo.clickOnScreen(mScreenWidth / 2, mScreenHeight - 1);
+		mSolo.clickOnScreen(mScreenWidth / 2, mScreenHeight - 10);
 		// mSolo.sleep(2000);
 		// mSolo.clickOnScreen(20, 60);
 		// assertTrue("Waiting for Dialog to close", mSolo.waitForDialogToClose(TIMEOUT));
@@ -187,7 +187,7 @@ public class ColorDialogIntegrationTest extends BaseIntegrationTestClass {
 		TypedArray presetColors = getActivity().getResources().obtainTypedArray(R.array.preset_colors);
 
 		mSolo.clickOnButton(presetColors.length() / 2);
-		mSolo.clickOnScreen(mScreenWidth / 2, mScreenHeight - 1);
+		mSolo.clickOnScreen(mScreenWidth / 2, mScreenHeight - 10);
 
 		assertTrue("Waiting for DrawingSurface", mSolo.waitForView(DrawingSurfaceImplementation.class, 1, TIMEOUT));
 		int newColor = mToolbar.getCurrentTool().getDrawPaint().getColor();
