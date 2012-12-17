0aa4d433286e09e84e56a7bfeba46c2b009dab4c
markusschmid <markusschmid@cp21-70.tu-graz.ac.at>
2012-12-17 10:32:52 +0100

    testing with sleep

--- PaintroidTest/src/org/catrobat/paintroid/test/integration/ColorDialogIntegrationTest.java
+++ PaintroidTest/src/org/catrobat/paintroid/test/integration/ColorDialogIntegrationTest.java
@@ -175,7 +175,7 @@ public class ColorDialogIntegrationTest extends BaseIntegrationTestClass {
 		mSolo.clickOnView(mMenuBottomParameter2);
 		assertTrue("Waiting for DrawingSurface", mSolo.waitForText(mSolo.getString(R.string.ok), 1, TIMEOUT * 2));
 		mSolo.clickOnScreen(1, mScreenHeight / 2);
-		// mSolo.sleep(2000);
+		mSolo.sleep(2000);
 		// mSolo.clickOnScreen(20, 60);
 		// assertTrue("Waiting for Dialog to close", mSolo.waitForDialogToClose(TIMEOUT));
 		assertTrue("Waiting for DrawingSurface", mSolo.waitForView(DrawingSurfaceImplementation.class, 1, TIMEOUT));
