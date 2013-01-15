fbf36c07a22686151d7fa13f25c3cf76c9c8551f
markusschmid <markusschmid@cp23-167.tu-graz.ac.at>
2013-01-15 15:05:16 +0100

    tests for disabled buttons, drawable addes

--- PaintroidTest/src/org/catrobat/paintroid/test/integration/CropToolIntegrationTest.java
+++ PaintroidTest/src/org/catrobat/paintroid/test/integration/CropToolIntegrationTest.java
@@ -43,7 +43,8 @@ public class CropToolIntegrationTest extends BaseIntegrationTestClass {
 	private final int CROPPING_SLEEP_BETWEEN_FINISH_CHECK = 500;
 	private final int MAXIMUM_CROPPING_TIMEOUT_COUNTS = 300;
 	private int mLineLength;
-	private Bitmap mCurrentDrawingSurfaceBitmap;
+
+	// private Bitmap mCurrentDrawingSurfaceBitmap;
 
 	public CropToolIntegrationTest() throws Exception {
 		super();
@@ -53,13 +54,11 @@ public class CropToolIntegrationTest extends BaseIntegrationTestClass {
 	@Before
 	protected void setUp() {
 		super.setUp();
-		try {
-			mCurrentDrawingSurfaceBitmap = (Bitmap) PrivateAccess.getMemberValue(DrawingSurfaceImplementation.class,
-					PaintroidApplication.DRAWING_SURFACE, "mWorkingBitmap");
-		} catch (Exception whatever) {
-			whatever.printStackTrace();
-			fail(whatever.toString());
-		}
+		/*
+		 * try { mCurrentDrawingSurfaceBitmap = (Bitmap)
+		 * PrivateAccess.getMemberValue(DrawingSurfaceImplementation.class, PaintroidApplication.DRAWING_SURFACE,
+		 * "mWorkingBitmap"); } catch (Exception whatever) { whatever.printStackTrace(); fail(whatever.toString()); }
+		 */
 
 		mLineLength = (mCurrentDrawingSurfaceBitmap.getWidth() / 2);
 	}
---
+++ PaintroidTest/src/org/catrobat/paintroid/test/integration/DisableButtonsTest.java
@@ -0,0 +1,36 @@
+package org.catrobat.paintroid.test.integration;
+
+
+public class DisableButtonsTest extends BaseIntegrationTestClass {
+
+	public DisableButtonsTest() throws Exception {
+		super();
+	}
+
+	/*
+	 * public void testDisableRedoUndoAfterStart() { assertEquals("Undo Button not disabled", mButtonTopUndo,
+	 * (ImageButton) getActivity().findViewById(R.id.btn_status_undo_disabled));
+	 * assertEquals("Redo Button not disabled", mButtonTopRedo, (ImageButton)
+	 * getActivity().findViewById(R.id.btn_status_redo_disabled));
+	 * 
+	 * }
+	 * 
+	 * public void testDisableUndoRedo() { mCurrentDrawingSurfaceBitmap.setPixel(mCurrentDrawingSurfaceBitmap.getWidth()
+	 * / 2, mCurrentDrawingSurfaceBitmap.getHeight() / 2, Color.BLACK); // update mButtonTopUndo = (ImageButton)
+	 * getActivity().findViewById(R.id.btn_status_undo); mButtonTopRedo = (ImageButton)
+	 * getActivity().findViewById(R.drawable.);
+	 * 
+	 * assertEquals("Undo Button stil disabled", mButtonTopUndo, getActivity().findViewById(R.id.btn_status_undo));
+	 * assertEquals("Redo Button not disabled", mButtonTopRedo, (ImageButton)
+	 * getActivity().findViewById(R.id.btn_status_redo_disabled));
+	 * 
+	 * mSolo.clickOnView(mButtonTopUndo);
+	 * 
+	 * // update mButtonTopUndo = (ImageButton) getActivity().findViewById(R.id.btn_status_undo); mButtonTopRedo =
+	 * (ImageButton) getActivity().findViewById(R.id.btn_status_redo);
+	 * 
+	 * assertEquals("Undo Button not disabled", mButtonTopUndo, (ImageButton)
+	 * getActivity().findViewById(R.id.btn_status_undo_disabled)); assertEquals("Redo Button not disabled",
+	 * mButtonTopRedo, (ImageButton) getActivity().findViewById(R.id.btn_status_redo_disabled)); }
+	 */
+}
