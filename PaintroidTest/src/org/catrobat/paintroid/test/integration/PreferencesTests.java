d2d078c27336294d2deb21d8caf9b08439c0eed3
Michael Muenzer <michael.muenzer@student.tugraz.at>
2013-01-31 22:26:58 +0100

    Fixed testcase for preferences

--- PaintroidTest/src/org/catrobat/paintroid/test/integration/PreferencesTests.java
+++ PaintroidTest/src/org/catrobat/paintroid/test/integration/PreferencesTests.java
@@ -148,10 +148,11 @@ public class PreferencesTests extends BaseIntegrationTestClass {
 		final String[] allTools = new String[] { mSolo.getString(R.string.button_brush),
 				mSolo.getString(R.string.button_cursor), mSolo.getString(R.string.button_pipette),
 				mSolo.getString(R.string.button_magic), mSolo.getString(R.string.button_stamp),
-				mSolo.getString(R.string.button_import_image), mSolo.getString(R.string.button_crop),
-				mSolo.getString(R.string.button_eraser), mSolo.getString(R.string.button_flip),
-				mSolo.getString(R.string.button_move), mSolo.getString(R.string.button_zoom),
-				mSolo.getString(R.string.button_undo), mSolo.getString(R.string.button_redo) };
+				mSolo.getString(R.string.button_rectangle), mSolo.getString(R.string.button_import_image),
+				mSolo.getString(R.string.button_crop), mSolo.getString(R.string.button_eraser),
+				mSolo.getString(R.string.button_flip), mSolo.getString(R.string.button_move),
+				mSolo.getString(R.string.button_zoom), mSolo.getString(R.string.button_undo),
+				mSolo.getString(R.string.button_redo) };
 		return allTools;
 	}
 
