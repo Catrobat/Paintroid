b8536fe8a79e5f7739c070c78b92d44de8e5fa11
Michael Muenzer <michael.muenzer@student.tugraz.at>
2013-02-05 18:02:06 +0100

    Implemented FloodFill for FillTool and FillCommand

--- Paintroid/src/org/catrobat/paintroid/command/implementation/FillCommand.java
+++ Paintroid/src/org/catrobat/paintroid/command/implementation/FillCommand.java
@@ -1,17 +1,76 @@
 package org.catrobat.paintroid.command.implementation;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Point;
 
 public class FillCommand extends BaseCommand {
 
 	private Point mClickedPixel;
 
-	@Override
-	public void run(Canvas canvas, Bitmap bitmap) {
-		// TODO Auto-generated method stub
-
+	public FillCommand(Point clickedPixel, Paint currentPaint) {
+		super(currentPaint);
+		mClickedPixel = clickedPixel;
 	}
 
+	@Override
+	public void run(Canvas canvas, Bitmap bitmap) {
+		setChanged();
+		notifyStatus(NOTIFY_STATES.COMMAND_STARTED);
+		if (mClickedPixel == null) {
+			setChanged();
+			notifyStatus(NOTIFY_STATES.COMMAND_FAILED);
+			return;
+		}
+
+		int replacementColor = bitmap
+				.getPixel(mClickedPixel.x, mClickedPixel.y);
+		fillAreaWithColor(bitmap, mClickedPixel, mPaint.getColor(),
+				replacementColor);
+
+		setChanged();
+		notifyStatus(NOTIFY_STATES.COMMAND_DONE);
+	}
+
+	private void fillAreaWithColor(Bitmap bmp, Point pt, int targetColor,
+			int replacementColor) {
+		Queue<Point> q = new LinkedList<Point>();
+		q.add(pt);
+		while (q.size() > 0) {
+			Point n = q.poll();
+			if (bmp.getPixel(n.x, n.y) != targetColor) {
+				continue;
+			}
+
+			Point w = n, e = new Point(n.x + 1, n.y);
+			while ((w.x > 0) && (bmp.getPixel(w.x, w.y) == targetColor)) {
+				bmp.setPixel(w.x, w.y, replacementColor);
+				if ((w.y > 0) && (bmp.getPixel(w.x, w.y - 1) == targetColor)) {
+					q.add(new Point(w.x, w.y - 1));
+				}
+				if ((w.y < bmp.getHeight() - 1)
+						&& (bmp.getPixel(w.x, w.y + 1) == targetColor)) {
+					q.add(new Point(w.x, w.y + 1));
+				}
+				w.x--;
+			}
+			while ((e.x < bmp.getWidth() - 1)
+					&& (bmp.getPixel(e.x, e.y) == targetColor)) {
+				bmp.setPixel(e.x, e.y, replacementColor);
+
+				if ((e.y > 0) && (bmp.getPixel(e.x, e.y - 1) == targetColor)) {
+					q.add(new Point(e.x, e.y - 1));
+				}
+				if ((e.y < bmp.getHeight() - 1)
+						&& (bmp.getPixel(e.x, e.y + 1) == targetColor)) {
+					q.add(new Point(e.x, e.y + 1));
+				}
+				e.x++;
+			}
+		}
+	}
 }
--- Paintroid/src/org/catrobat/paintroid/tools/implementation/FillTool.java
+++ Paintroid/src/org/catrobat/paintroid/tools/implementation/FillTool.java
@@ -1,7 +1,12 @@
 package org.catrobat.paintroid.tools.implementation;
 
+import org.catrobat.paintroid.PaintroidApplication;
+import org.catrobat.paintroid.command.Command;
+import org.catrobat.paintroid.command.implementation.FillCommand;
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.graphics.PointF;
 
 public class FillTool extends BaseTool {
@@ -22,8 +27,13 @@ public class FillTool extends BaseTool {
 
 	@Override
 	public boolean handleUp(PointF coordinate) {
-		// TODO Auto-generated method stub
-		return false;
+		Command command = new FillCommand(new Point((int) coordinate.x,
+				(int) coordinate.y), mBitmapPaint);
+		mProgressDialog.show();
+		((FillCommand) command).addObserver(this);
+		PaintroidApplication.COMMAND_MANAGER.commitCommand(command);
+
+		return true;
 	}
 
 	@Override
