3bbdf9f19a14d6066861dd7a80ae299be0817c5f
Stefan Galler <galler.stefan@gmail.com>
2013-02-06 11:05:10 +0100

    work algorithm with pixel array

--- Paintroid/src/org/catrobat/paintroid/tools/implementation/FillTool.java
+++ Paintroid/src/org/catrobat/paintroid/tools/implementation/FillTool.java
@@ -1,8 +1,10 @@
 package org.catrobat.paintroid.tools.implementation;
 
 import org.catrobat.paintroid.PaintroidApplication;
+import org.catrobat.paintroid.R;
 import org.catrobat.paintroid.command.Command;
 import org.catrobat.paintroid.command.implementation.FillCommand;
+import org.catrobat.paintroid.ui.button.ToolbarButton.ToolButtonIDs;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -27,6 +29,20 @@ public class FillTool extends BaseTool {
 
 	@Override
 	public boolean handleUp(PointF coordinate) {
+
+		int bitmapHeight = PaintroidApplication.DRAWING_SURFACE
+				.getBitmapHeight();
+		int bitmapWidth = PaintroidApplication.DRAWING_SURFACE.getBitmapWidth();
+
+		if ((coordinate.x > bitmapWidth) || (coordinate.y > bitmapHeight)) {
+			return false;
+		}
+
+		if (mBitmapPaint.getColor() == PaintroidApplication.DRAWING_SURFACE
+				.getBitmapColor(coordinate)) {
+			return false;
+		}
+
 		Command command = new FillCommand(new Point((int) coordinate.x,
 				(int) coordinate.y), mBitmapPaint);
 		mProgressDialog.show();
@@ -34,6 +50,29 @@ public class FillTool extends BaseTool {
 		PaintroidApplication.COMMAND_MANAGER.commitCommand(command);
 
 		return true;
+	}
+
+	@Override
+	public int getAttributeButtonResource(ToolButtonIDs buttonNumber) {
+		switch (buttonNumber) {
+		case BUTTON_ID_PARAMETER_BOTTOM_2:
+			return R.drawable.icon_menu_color_palette;
+		default:
+			return super.getAttributeButtonResource(buttonNumber);
+		}
+
+	}
+
+	@Override
+	public void attributeButtonClick(ToolButtonIDs buttonNumber) {
+		switch (buttonNumber) {
+		case BUTTON_ID_PARAMETER_TOP:
+		case BUTTON_ID_PARAMETER_BOTTOM_2:
+			showColorPicker();
+			break;
+		default:
+			super.attributeButtonClick(buttonNumber);
+		}
 	}
 
 	@Override
