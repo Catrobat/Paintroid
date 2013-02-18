67722ae0af35661452e9aa5e8d69c9ca11741ad4
Johannes Max Loeffler <johloe@sbox.tugraz.at>
2013-02-18 10:24:55 +0100

    fresh start

--- Paintroid/src/org/catrobat/paintroid/tools/implementation/StampTool.java
+++ Paintroid/src/org/catrobat/paintroid/tools/implementation/StampTool.java
@@ -43,11 +43,17 @@ public class StampTool extends BaseToolWithRectangleShape {
 	private static final boolean ROTATION_ENABLED = true;
 	private static final boolean RESPECT_IMAGE_BOUNDS = false;
 
+	private boolean mStampActive = false;
+
 	public StampTool(Context context, ToolType toolType) {
 		super(context, toolType);
 
+		mStampActive = false;
 		setRotationEnabled(ROTATION_ENABLED);
 		setRespectImageBounds(RESPECT_IMAGE_BOUNDS);
+
+		mDrawingBitmap = Bitmap.createBitmap((int) mBoxWidth, (int) mBoxHeight,
+				Config.ARGB_8888);
 	}
 
 	@Override
@@ -65,7 +71,82 @@ public class StampTool extends BaseToolWithRectangleShape {
 		// no clicks wanted
 	}
 
+	private void createAndSetBitmapRotated(DrawingSurface drawingSurface) {
+		float boxRot = mBoxRotation;
+
+		while (boxRot < 0.0) {
+			boxRot = boxRot + 90;
+		}
+
+		while (boxRot > 90) {
+			boxRot = boxRot - 90;
+		}
+
+		double rotationRadians = Math.toRadians(boxRot);
+		double a = mBoxWidth * Math.sin(rotationRadians) + mBoxHeight
+				* Math.cos(rotationRadians);
+
+		double b = mBoxWidth * Math.cos(rotationRadians) + mBoxHeight
+				* Math.sin(rotationRadians);
+
+		if (a < 0.0) {
+			a = -a;
+		}
+
+		if (b < 0.0) {
+			b = -b;
+		}
+
+		Bitmap tmpBitmap = Bitmap.createBitmap((int) a, (int) b,
+				Config.ARGB_8888);
+
+		Canvas canvas = new Canvas(tmpBitmap);
+
+		Rect rectSource = new Rect((int) mToolPosition.x - (int) (a / 2),
+				(int) mToolPosition.y - (int) (b / 2), (int) mToolPosition.x
+						+ (int) (a / 2), (int) mToolPosition.y + (int) (b / 2));
+
+		Rect rectDest = new Rect(0, 0, (int) a, (int) b);
+
+		canvas.save();
+		canvas.rotate(-mBoxRotation, (float) (a / 2), (float) (b / 2));
+
+		canvas.drawBitmap(drawingSurface.getBitmap(), rectSource, rectDest,
+				null);
+
+		canvas.restore();
+
+		// now get tmp back to bitmap, rotate and clip
+		mDrawingBitmap = Bitmap.createBitmap((int) mBoxWidth, (int) mBoxHeight,
+				Config.ARGB_8888);
+
+		Canvas canvasDraw = new Canvas(mDrawingBitmap);
+
+		double left = (a / 2) - (mBoxWidth / 2);
+		double top = (b / 2) - (mBoxHeight / 2);
+		double right = a - left;
+		double bottom = b - top;
+		Rect rectSource2 = new Rect((int) left, (int) top, (int) right,
+				(int) bottom);
+
+		Rect rectDest2 = new Rect(0, 0, (int) mBoxWidth, (int) mBoxHeight);
+
+		canvasDraw.drawBitmap(tmpBitmap, rectSource2, rectDest2, null);
+
+		mStampActive = true;
+
+	}
+
 	protected void createAndSetBitmap(DrawingSurface drawingSurface) {
+		if (mDrawingBitmap != null) {
+			mDrawingBitmap.recycle();
+			mDrawingBitmap = null;
+		}
+
+		if (mBoxRotation != 0.0) {
+			createAndSetBitmapRotated(drawingSurface);
+			return;
+		}
 
 		mDrawingBitmap = Bitmap.createBitmap((int) mBoxWidth, (int) mBoxHeight,
 				Config.ARGB_8888);
@@ -90,6 +171,8 @@ public class StampTool extends BaseToolWithRectangleShape {
 			canvas.drawBitmap(drawingSurface.getBitmap(), rectSource, rectDest,
 					null);
 
+			mStampActive = true;
+
 			Log.d(PaintroidApplication.TAG, "created bitmap");
 		} catch (Exception e) {
 			Log.e(PaintroidApplication.TAG,
@@ -104,7 +187,7 @@ public class StampTool extends BaseToolWithRectangleShape {
 
 	@Override
 	protected void onClickInBox() {
-		if (mDrawingBitmap == null) {
+		if (mStampActive == false) {
 			mProgressDialog.show();
 			createAndSetBitmap(PaintroidApplication.DRAWING_SURFACE);
 			mProgressDialog.dismiss();
