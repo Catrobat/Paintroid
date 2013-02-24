5ee0400b3a173744ea930ae24b54496fc55b3a03
Stefan Galler <galler.stefan@gmail.com>
2013-02-24 14:22:15 +0100

    show transparency in color button

--- Paintroid/src/org/catrobat/paintroid/ui/button/ColorButton.java
+++ Paintroid/src/org/catrobat/paintroid/ui/button/ColorButton.java
@@ -1,13 +1,18 @@
 package org.catrobat.paintroid.ui.button;
 
+import org.catrobat.paintroid.R;
 import org.catrobat.paintroid.dialog.colorpicker.ColorPickerDialog;
 import org.catrobat.paintroid.dialog.colorpicker.ColorPickerDialog.OnColorPickedListener;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Shader.TileMode;
 import android.util.AttributeSet;
 import android.widget.ImageButton;
 
@@ -19,23 +24,35 @@ public class ColorButton extends ImageButton implements OnColorPickedListener {
 
 	private Paint mColorPaint;
 	private Paint mBorderPaint;
+	private Paint mBackgroundPaint;
+	private Bitmap mBackgroundBitmap;
+
 	private int mHeigth;
 	private int mWidth;
 
 	public ColorButton(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		init();
+		init(context);
 	}
 
-	private void init() {
+	private void init(Context context) {
 		mColorPaint = new Paint();
+		mBackgroundPaint = new Paint();
 		mBorderPaint = new Paint();
 		mBorderPaint.setColor(RECT_BORDER_COLOR);
+
+		mBackgroundBitmap = BitmapFactory.decodeResource(getResources(),
+				R.drawable.checkeredbg);
+		BitmapShader backgroundShader = new BitmapShader(mBackgroundBitmap,
+				TileMode.REPEAT, TileMode.REPEAT);
+		mBackgroundPaint.setShader(backgroundShader);
+
 		ColorPickerDialog.getInstance().addOnColorPickedListener(this);
 	}
 
 	@Override
 	public void colorChanged(int color) {
+
 		mColorPaint.setColor(color);
 		invalidate();
 	}
@@ -50,8 +67,11 @@ public class ColorButton extends ImageButton implements OnColorPickedListener {
 		Rect borderRect = new Rect(colorRect.left - RECT_BORDER_SIZE,
 				colorRect.top - RECT_BORDER_SIZE, colorRect.right
 						+ RECT_BORDER_SIZE, colorRect.bottom + RECT_BORDER_SIZE);
+
 		canvas.drawRect(borderRect, mBorderPaint);
+		canvas.drawRect(colorRect, mBackgroundPaint);
 		canvas.drawRect(colorRect, mColorPaint);
+
 	}
 
 	@Override
