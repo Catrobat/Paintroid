c9e8904379228f3dbc2b975811984908d4a981e0
Stefan Galler <galler.stefan@gmail.com>
2013-02-25 11:04:08 +0100

    update immediately after color change

--- Paintroid/src/org/catrobat/paintroid/dialog/colorpicker/ColorPickerDialog.java
+++ Paintroid/src/org/catrobat/paintroid/dialog/colorpicker/ColorPickerDialog.java
@@ -120,6 +120,7 @@ public final class ColorPickerDialog extends BaseDialog {
 					@Override
 					public void colorChanged(int color) {
 						changeNewColor(color);
+						updateColorChange(color);
 					}
 				});
 
