c1f8c536e351157850ffca34c1f2b16daa4ad96b
Gerald Musser <gerald@musser.ws>
2013-03-21 20:28:16 +0100

    icon update

--- Paintroid/src/org/catrobat/paintroid/tools/implementation/CropTool.java
+++ Paintroid/src/org/catrobat/paintroid/tools/implementation/CropTool.java
@@ -158,9 +158,9 @@ public class CropTool extends BaseToolWithRectangleShape {
 		case BUTTON_ID_PARAMETER_TOP:
 			return NO_BUTTON_RESOURCE;
 		case BUTTON_ID_PARAMETER_BOTTOM_1:
-			return R.drawable.icon_crop;
+			return R.drawable.icon_menu_crop_adjust;
 		case BUTTON_ID_PARAMETER_BOTTOM_2:
-			return R.drawable.icon_content_cut;
+			return R.drawable.icon_menu_crop_cut;
 		default:
 			return super.getAttributeButtonResource(buttonNumber);
 		}
