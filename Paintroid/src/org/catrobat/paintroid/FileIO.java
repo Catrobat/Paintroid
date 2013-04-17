86d12d1ea837c3a3c78d1c0a37c09052d76d2fd4
Johannes Max Loeffler <johloe@sbox.tugraz.at>
2013-04-17 11:31:41 +0200

    license text updated, in Paintroidproject, in *.java

--- Paintroid/src/org/catrobat/paintroid/FileIO.java
+++ Paintroid/src/org/catrobat/paintroid/FileIO.java
@@ -1,16 +1,12 @@
 /**
- *  Catroid: An on-device visual programming system for Android devices
- *  Copyright (C) 2010-2012 The Catrobat Team
+ *  Paintroid: An image manipulation application for Android.
+ *  Copyright (C) 2010-2013 The Catrobat Team
  *  (<http://developer.catrobat.org/credits>)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
  *  published by the Free Software Foundation, either version 3 of the
  *  License, or (at your option) any later version.
- *
- *  An additional term exception under section 7 of the GNU Affero
- *  General Public License, version 3, is available at
- *  http://www.catroid.org/catroid/licenseadditionalterm
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -192,8 +188,8 @@ public abstract class FileIO {
 		return filepath;
 	}
 
-	public static void copyStream(InputStream inputStream, OutputStream outputStream)
-			throws IOException {
+	public static void copyStream(InputStream inputStream,
+			OutputStream outputStream) throws IOException {
 		byte[] buffer = new byte[BUFFER_SIZE];
 		int bytesRead;
 		while ((bytesRead = inputStream.read(buffer)) != -1) {
