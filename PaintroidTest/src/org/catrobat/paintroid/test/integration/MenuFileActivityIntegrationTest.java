c6d8c9f7e13c8586765fd5e1e8a1ea787296d666
Stefan Galler <galler.stefan@gmail.com>
2013-06-13 09:22:01 +0200

    fixed tests

--- PaintroidTest/src/org/catrobat/paintroid/test/integration/MenuFileActivityIntegrationTest.java
+++ PaintroidTest/src/org/catrobat/paintroid/test/integration/MenuFileActivityIntegrationTest.java
@@ -66,7 +66,8 @@ public class MenuFileActivityIntegrationTest extends BaseIntegrationTestClass {
 	public void testNewEmptyDrawing() {
 		final int xCoordinatePixel = 0;
 		final int yCoordinatePixel = 0;
-		assertTrue("Waiting for DrawingSurface", mSolo.waitForView(DrawingSurface.class, 1, TIMEOUT));
+
+		mSolo.clickOnScreen(mScreenWidth / 2, mScreenHeight / 2);
 
 		mCurrentDrawingSurfaceBitmap.setPixel(xCoordinatePixel, yCoordinatePixel, Color.BLACK);
 
@@ -92,6 +93,7 @@ public class MenuFileActivityIntegrationTest extends BaseIntegrationTestClass {
 	}
 
 	public void testLoadImageDialogOnBackPressed() {
+		mSolo.clickOnScreen(mScreenWidth / 2, mScreenHeight / 2);
 		mSolo.clickOnMenuItem(mSolo.getString(R.string.menu_load_image));
 		mSolo.waitForActivity("AlertActivity", TIMEOUT);
 		mSolo.goBack();
@@ -121,6 +123,8 @@ public class MenuFileActivityIntegrationTest extends BaseIntegrationTestClass {
 	// }
 
 	public void testWarningDialogOnNewImageFromCamera() {
+
+		mSolo.clickOnScreen(mScreenWidth / 2, mScreenHeight / 2);
 
 		boolean tryContensedString = false;
 		try {
@@ -167,6 +171,8 @@ public class MenuFileActivityIntegrationTest extends BaseIntegrationTestClass {
 	public void testNewDrawingDialogOnBackPressed() {
 		final int xCoordinatePixel = 0;
 		final int yCoordinatePixel = 0;
+
+		mSolo.clickOnScreen(mScreenWidth / 2, mScreenHeight / 2);
 
 		mCurrentDrawingSurfaceBitmap.setPixel(xCoordinatePixel, yCoordinatePixel, Color.BLACK);
 		assertEquals("Color on drawing surface wrong", Color.BLACK,
