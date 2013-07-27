76553d4acb2e7eb887988c9148d1e95714e36e28
X4mp <urbano.roland@gmail.com>
2013-07-27 14:21:28 +0200

    added Testcase save image different name

--- PaintroidTest/src/org/catrobat/paintroid/test/integration/MenuFileActivityIntegrationTest.java
+++ PaintroidTest/src/org/catrobat/paintroid/test/integration/MenuFileActivityIntegrationTest.java
@@ -374,8 +374,7 @@ public class MenuFileActivityIntegrationTest extends BaseIntegrationTestClass {
 
 		final int newXCoordinatePixel = 100;
 		final int newYCoordinatePixel = 150;
-		// mCurrentDrawingSurfaceBitmap.setPixel(newXCoordinatePixel, newYCoordinatePixel, Color.BLACK);
-		mSolo.clickOnScreen(newXCoordinatePixel, newYCoordinatePixel);
+		mCurrentDrawingSurfaceBitmap.setPixel(newXCoordinatePixel, newYCoordinatePixel, Color.BLACK);
 		assertEquals("Color on drawing surface wrong", Color.BLACK,
 				PaintroidApplication.drawingSurface.getPixel(new PointF(newXCoordinatePixel, newYCoordinatePixel)));
 
@@ -392,6 +391,54 @@ public class MenuFileActivityIntegrationTest extends BaseIntegrationTestClass {
 		assertTrue("actual file should be bigger", newFileLength > oldFileLength);
 		assertTrue("image should be deleted", imageFile.delete());
 
+	}
+
+	public void testSaveLoadedImageCorrectFileNameNoOverride() {
+		final int xCoordinatePixel = 100;
+		final int yCoordinatePixel = 50;
+
+		assertTrue("Waiting for DrawingSurface", mSolo.waitForView(DrawingSurface.class, 1, TIMEOUT));
+
+		mCurrentDrawingSurfaceBitmap.setPixel(xCoordinatePixel, yCoordinatePixel, Color.BLACK); //
+		mSolo.clickOnScreen(xCoordinatePixel, yCoordinatePixel);
+		assertEquals("Color on drawing surface wrong", Color.BLACK,
+				PaintroidApplication.drawingSurface.getPixel(new PointF(xCoordinatePixel, yCoordinatePixel)));
+
+		FileIO.saveBitmap(getActivity(), PaintroidApplication.drawingSurface.getBitmapCopy(),
+				FILENAMES.get(CORRECT_FILENAME_INDEX));
+
+		File imageFile = getImageFile(FILENAMES.get(CORRECT_FILENAME_INDEX));
+		long oldFileLength = imageFile.length();
+
+		PaintroidApplication.loadedFileName = FILENAMES.get(CORRECT_FILENAME_INDEX);
+		PaintroidApplication.loadedFilePath = imageFile.getAbsolutePath();
+
+		final int newXCoordinatePixel = 100;
+		final int newYCoordinatePixel = 150;
+		mCurrentDrawingSurfaceBitmap.setPixel(newXCoordinatePixel, newYCoordinatePixel, Color.BLACK);
+		assertEquals("Color on drawing surface wrong", Color.BLACK,
+				PaintroidApplication.drawingSurface.getPixel(new PointF(newXCoordinatePixel, newYCoordinatePixel)));
+
+		mSolo.clickOnMenuItem(mSolo.getString(R.string.menu_save_image));
+		assertTrue("wait for overwrite question",
+				mSolo.waitForText(mSolo.getString(R.string.dialog_overwrite_text), 1, TIMEOUT));
+		mSolo.clickOnText(mSolo.getString(R.string.no));
+
+		EditText editText = (EditText) mSolo.getView(R.id.dialog_save_file_edit_text);
+		FILENAMES.add(editText.getHint().toString());
+
+		mSolo.enterText(editText, FILENAMES.get(CORRECT_FILENAME_INDEX));
+		mSolo.clickOnText(mSolo.getString(R.string.ok));
+		assertTrue("wait for overwrite question",
+				mSolo.waitForText(mSolo.getString(R.string.dialog_overwrite_text), 1, TIMEOUT));
+		mSolo.clickOnButton(mSolo.getString(R.string.yes));
+		assertTrue("Waiting for DrawingSurface", mSolo.waitForView(DrawingSurface.class, 1, TIMEOUT));
+		imageFile = getImageFile(FILENAMES.get(CORRECT_FILENAME_INDEX));
+		assertTrue("image file should exist", imageFile.exists());
+		long newFileLength = imageFile.length();
+		assertTrue("actual file should be bigger", newFileLength > oldFileLength);
+
+		assertTrue("image should be deleted", imageFile.delete());
 	}
 
 	private File getImageFile(String filename) {
