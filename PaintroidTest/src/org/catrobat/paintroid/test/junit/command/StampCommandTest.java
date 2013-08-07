35ece22e3e5fbcac53ca42ffc9ff6f03ca6c2f2a
X4mp <urbano.roland@gmail.com>
2013-08-07 19:05:12 +0200

    corrected testRun()

--- PaintroidTest/src/org/catrobat/paintroid/test/junit/command/StampCommandTest.java
+++ PaintroidTest/src/org/catrobat/paintroid/test/junit/command/StampCommandTest.java
@@ -56,7 +56,7 @@ public class StampCommandTest extends CommandTestSetup {
 
 	@Test
 	public void testRun() {
-		mCommandUnderTest.run(mCanvasUnderTest, null);
+		mCommandUnderTest.run(mCanvasUnderTest, Bitmap.createBitmap(1, 1, Config.ARGB_8888));
 		PaintroidAsserts.assertBitmapEquals(mStampBitmapUnderTest, mCanvasBitmapUnderTest);
 
 		try {
