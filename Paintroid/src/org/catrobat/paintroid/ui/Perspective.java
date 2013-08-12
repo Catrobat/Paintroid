67d831c16daf93ee07de069400bacac1e539b2ac
Johannes Max Loeffler <johloe@sbox.tugraz.at>
2013-08-12 14:08:19 +0200

    del Log.e

--- Paintroid/src/org/catrobat/paintroid/ui/Perspective.java
+++ Paintroid/src/org/catrobat/paintroid/ui/Perspective.java
@@ -30,7 +30,6 @@ import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.WindowManager;
@@ -72,7 +71,6 @@ public class Perspective implements Serializable {
 				.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
 		display.getMetrics(metrics);
 		mScreenDensity = metrics.density;
-		Log.e("IHATEJENKINS", "" + mScreenDensity);
 		mIsFullscreen = false;
 	}
 
