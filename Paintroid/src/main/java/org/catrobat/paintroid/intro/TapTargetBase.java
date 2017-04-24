6e71f9074b48410f789aea1c6f1c0387af116f22
Clemens Stary <c.stary@student.tugraz.at>
2017-04-24 14:34:27 +0200

    Bugfix Class Scope

--- Paintroid/src/main/java/org/catrobat/paintroid/intro/TapTargetBase.java
+++ Paintroid/src/main/java/org/catrobat/paintroid/intro/TapTargetBase.java
@@ -42,7 +42,7 @@ import java.util.LinkedHashMap;
 import static org.catrobat.paintroid.intro.helper.IntroAnimationHelper.fadeOut;
 import static org.catrobat.paintroid.intro.helper.WelcomeActivityHelper.getDpFromInt;
 
-abstract class TapTargetBase {
+public abstract class TapTargetBase {
     protected final static String TAG = "TapTarget";
     private static final int RADIUS_OFFSET = 2;
     protected final Context context;
