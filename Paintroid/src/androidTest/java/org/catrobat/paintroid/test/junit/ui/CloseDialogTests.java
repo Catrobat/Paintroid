5d2312d88bceda96a945bbbc0e8f0c4d1b0b0fbd
Christian Benkovic <christian.benkovic@student.tugraz.at>
2017-05-22 17:46:53 +0200

    code formatting

--- Paintroid/src/androidTest/java/org/catrobat/paintroid/test/junit/ui/CloseDialogTests.java
+++ Paintroid/src/androidTest/java/org/catrobat/paintroid/test/junit/ui/CloseDialogTests.java
@@ -1,37 +1,30 @@
 /**
- *  Paintroid: An image manipulation application for Android.
- *  Copyright (C) 2010-2015 The Catrobat Team
- *  (<http://developer.catrobat.org/credits>)
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as
- *  published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Paintroid: An image manipulation application for Android.
+ * Copyright (C) 2010-2015 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.catrobat.paintroid.test.junit.ui;
 
-import android.support.test.espresso.NoMatchingViewException;
-import android.support.test.espresso.ViewAction;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.Gravity;
 
 import org.catrobat.paintroid.MainActivity;
-import org.catrobat.paintroid.PaintroidApplication;
 import org.catrobat.paintroid.R;
-import org.catrobat.paintroid.tools.ToolType;
-import org.catrobat.paintroid.ui.BottomBar;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,16 +32,12 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.doubleClick;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerActions.open;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
-import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
-import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.not;
 
@@ -56,65 +45,51 @@ import static org.hamcrest.Matchers.not;
 @RunWith(AndroidJUnit4.class)
 public class CloseDialogTests {
 
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule =
-            new ActivityTestRule<>(MainActivity.class);
+	@Rule
+	public ActivityTestRule<MainActivity> mActivityRule =
+			new ActivityTestRule<>(MainActivity.class);
 
 
-    @Test
-    public void testCloseNavigationDrawerOnBackPressed() {
-        onView(withId(R.id.drawer_layout)).perform(open());
-        onView(withId(R.id.drawer_layout)).check(matches(isDisplayed()));
-        pressBack();
-        onView (withId(R.id.drawer_layout)).check(matches(isClosed()));
-    }
+	@Test
+	public void testCloseNavigationDrawerOnBackPressed() {
+		onView(withId(R.id.drawer_layout)).perform(open());
+		onView(withId(R.id.drawer_layout)).check(matches(isDisplayed()));
+		pressBack();
+		onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
+	}
 
-    @Test
-    public void testCloseLayerDialogOnBackPressed() {
-        onView(withId(R.id.drawer_layout)).perform(open(Gravity.RIGHT));
-        onView(withId(R.id.drawer_layout)).check(matches(isDisplayed()));
-        pressBack();
-        onView (withId(R.id.drawer_layout)).check(matches(isClosed()));
+	@Test
+	public void testCloseLayerDialogOnBackPressed() {
+		onView(withId(R.id.drawer_layout)).perform(open(Gravity.RIGHT));
+		onView(withId(R.id.drawer_layout)).check(matches(isDisplayed()));
+		pressBack();
+		onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
 
-    }
+	}
 
-    @Test
-    public void testCloseColorPickerDialogOnBackPressed() {
-        onView(withId(R.id.btn_top_colorframe)).perform(click());
-        onView(withId(R.id.colorchooser_base_layout)).check(matches(isDisplayed()));
-        pressBack();
-        onView (withId(R.id.colorchooser_base_layout)).check(doesNotExist());
-    }
+	@Test
+	public void testCloseColorPickerDialogOnBackPressed() {
+		onView(withId(R.id.btn_top_colorframe)).perform(click());
+		onView(withId(R.id.colorchooser_base_layout)).check(matches(isDisplayed()));
+		pressBack();
+		onView(withId(R.id.colorchooser_base_layout)).check(doesNotExist());
+	}
 
 
-    @Test
-    public void testCloseToolOptionOnBackPressed() {
-        /*
-            DISABLE Animator duration scale in developer options
-            Recommendation: Turn off all system-animation in developer options
-                -Window animation scale
-                -Transition animation scale
-                -Animator duration scale
-         */
-        onView(withId(R.id.tools_rectangle)).perform(click());
-        onView(withId(R.id.layout_tool_options)).check((matches(isDisplayed())));
-        pressBack();
-        onView (withId(R.id.layout_tool_options)).check(matches(not(isDisplayed())));
-    }
+	@Test
+	public void testCloseToolOptionOnBackPressed() {
+		onView(withId(R.id.tools_rectangle)).perform(click());
+		onView(withId(R.id.layout_tool_options)).check((matches(isDisplayed())));
+		pressBack();
+		onView(withId(R.id.layout_tool_options)).check(matches(not(isDisplayed())));
+	}
 
-    @Test
-    public void testCloseToolOptionsOnUndoPressed() {
-        /*
-            DISABLE Animator duration scale in developer options
-            Recommendation: Turn off all system-animation in developer options
-                -Window animation scale
-                -Transition animation scale
-                -Animator duration scale
-         */
-        onView(withId(R.id.tools_text)).perform(scrollTo(), click());
-        onView(withId(R.id.layout_tool_options)).check((matches(isDisplayed())));
-        onView(withId(R.id.btn_top_undo)).perform(click());
-        onView (withId(R.id.layout_tool_options)).check(matches(not(isDisplayed())));
-    }
+	@Test
+	public void testCloseToolOptionsOnUndoPressed() {
+		onView(withId(R.id.tools_text)).perform(scrollTo(), click());
+		onView(withId(R.id.layout_tool_options)).check((matches(isDisplayed())));
+		onView(withId(R.id.btn_top_undo)).perform(click());
+		onView(withId(R.id.layout_tool_options)).check(matches(not(isDisplayed())));
+	}
 
 }
