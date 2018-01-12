051ce1aebbc171606439d3fa03cb7b46d35c5134
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2018-01-12 14:50:42 +0100

    PAINT-270: Move static undo/redo functions to `UndoRedoManager`

--- Paintroid/src/main/java/org/catrobat/paintroid/command/UndoRedoManager.java
+++ Paintroid/src/main/java/org/catrobat/paintroid/command/UndoRedoManager.java
@@ -33,9 +33,9 @@ import org.catrobat.paintroid.dialog.IndeterminateProgressDialog;
 import org.catrobat.paintroid.listener.LayerListener;
 import org.catrobat.paintroid.tools.Layer;
 import org.catrobat.paintroid.tools.Tool;
-import org.catrobat.paintroid.tools.implementation.TransformTool;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public final class UndoRedoManager {
 
@@ -49,6 +49,142 @@ public final class UndoRedoManager {
 			instance = new UndoRedoManager();
 		}
 		return instance;
+	}
+
+	private static void undoResizeCommand(Layer undoLayer, ResizeCommand undoCommand) {
+		for (Layer layer : LayerListener.getInstance().getAdapter().getLayers()) {
+			if (layer == undoLayer) {
+				continue;
+			}
+
+			LayerCommand layerCommand = new LayerCommand(layer);
+			LayerBitmapCommand layerBitmapCommand = PaintroidApplication.commandManager.getLayerBitmapCommand(layerCommand);
+			List<Command> layerCommands = layerBitmapCommand.getLayerCommands();
+
+			if (!layerCommands.isEmpty()) {
+				int indexOfLastElement = layerCommands.size() - 1;
+				Command lastCommand = layerCommands.get(indexOfLastElement);
+				if (lastCommand instanceof ResizeCommand) {
+					layerBitmapCommand.addCommandToUndoList();
+					LayerListener.getInstance().selectLayer(layer);
+					layerBitmapCommand.clearLayerBitmap();
+					layerBitmapCommand.runAllCommands();
+					continue;
+				}
+			}
+
+			int undoWidth = undoLayer.getImage().getWidth();
+			int undoHeight = undoLayer.getImage().getHeight();
+			int currentWidth = layer.getImage().getWidth();
+			int currentHeight = layer.getImage().getHeight();
+
+			Command resizeCommand = new ResizeCommand(
+					-undoCommand.getResizeCoordinateXLeft(),
+					-undoCommand.getResizeCoordinateYTop(),
+					currentWidth - (undoCommand.getResizeCoordinateXRight() - undoWidth),
+					currentHeight - (undoCommand.getResizeCoordinateYBottom() - undoHeight),
+					undoCommand.getMaximumBitmapResolution());
+
+			PaintroidApplication.commandManager.commitCommandToLayer(new LayerCommand(layer), resizeCommand);
+		}
+
+		if (!undoLayer.getSelected()) {
+			LayerListener.getInstance().selectLayer(undoLayer);
+		}
+	}
+
+	private static void redoResizeCommand(Layer redoLayer, ResizeCommand redoCommand) {
+		for (Layer layer : LayerListener.getInstance().getAdapter().getLayers()) {
+			if (layer == redoLayer) {
+				continue;
+			}
+
+			LayerCommand layerCommand = new LayerCommand(layer);
+			LayerBitmapCommand layerBitmapCommand = PaintroidApplication.commandManager.getLayerBitmapCommand(layerCommand);
+			List<Command> undoCommands = layerBitmapCommand.getLayerUndoCommands();
+
+			if (!undoCommands.isEmpty()) {
+				Command firstCommand = undoCommands.get(0);
+				if (firstCommand instanceof ResizeCommand) {
+					firstCommand.run(PaintroidApplication.drawingSurface.getCanvas(), layer);
+					layerBitmapCommand.addCommandToRedoList();
+					continue;
+				}
+			}
+
+			Command resizeCommand = new ResizeCommand(
+					redoCommand.getResizeCoordinateXLeft(),
+					redoCommand.getResizeCoordinateYTop(),
+					redoCommand.getResizeCoordinateXRight(),
+					redoCommand.getResizeCoordinateYBottom(),
+					redoCommand.getMaximumBitmapResolution());
+
+			PaintroidApplication.commandManager.commitCommandToLayer(new LayerCommand(layer), resizeCommand);
+		}
+	}
+
+	private static void undoRotateCommand(Layer undoLayer, RotateCommand undoCommand) {
+		for (Layer layer : LayerListener.getInstance().getAdapter().getLayers()) {
+			if (layer == undoLayer) {
+				continue;
+			}
+
+			LayerCommand layerCommand = new LayerCommand(layer);
+			LayerBitmapCommand layerBitmapCommand = PaintroidApplication.commandManager.getLayerBitmapCommand(layerCommand);
+			List<Command> layerCommands = layerBitmapCommand.getLayerCommands();
+
+			if (!layerCommands.isEmpty()) {
+				int indexOfLastElement = layerCommands.size() - 1;
+				Command lastCommand = layerCommands.get(indexOfLastElement);
+				if (lastCommand instanceof RotateCommand) {
+					layerBitmapCommand.addCommandToUndoList();
+					LayerListener.getInstance().selectLayer(layer);
+					layerBitmapCommand.clearLayerBitmap();
+					layerBitmapCommand.runAllCommands();
+					continue;
+				}
+			}
+
+			RotateCommand.RotateDirection rotateDirection = null;
+			switch (undoCommand.getRotateDirection()) {
+				case ROTATE_LEFT:
+					rotateDirection = RotateCommand.RotateDirection.ROTATE_RIGHT;
+					break;
+				case ROTATE_RIGHT:
+					rotateDirection = RotateCommand.RotateDirection.ROTATE_LEFT;
+			}
+
+			Command rotateCommand = new RotateCommand(rotateDirection);
+			PaintroidApplication.commandManager.commitCommandToLayer(new LayerCommand(layer), rotateCommand);
+		}
+
+		if (!undoLayer.getSelected()) {
+			LayerListener.getInstance().selectLayer(undoLayer);
+		}
+	}
+
+	private static void redoRotateCommand(Layer redoLayer, RotateCommand redoCommand) {
+		for (Layer layer : LayerListener.getInstance().getAdapter().getLayers()) {
+			if (layer == redoLayer) {
+				continue;
+			}
+
+			LayerCommand layerCommand = new LayerCommand(layer);
+			LayerBitmapCommand layerBitmapCommand = PaintroidApplication.commandManager.getLayerBitmapCommand(layerCommand);
+			List<Command> undoCommands = layerBitmapCommand.getLayerUndoCommands();
+
+			if (!undoCommands.isEmpty()) {
+				Command firstCommand = undoCommands.get(0);
+				if (firstCommand instanceof RotateCommand) {
+					firstCommand.run(PaintroidApplication.drawingSurface.getCanvas(), layer);
+					layerBitmapCommand.addCommandToRedoList();
+					continue;
+				}
+			}
+
+			Command rotateCommand = new RotateCommand(redoCommand.getRotateDirection());
+			PaintroidApplication.commandManager.commitCommandToLayer(new LayerCommand(layer), rotateCommand);
+		}
 	}
 
 	public void performUndo() {
@@ -122,9 +258,9 @@ public final class UndoRedoManager {
 						if (!layerBitmapCommand.getLayerUndoCommands().isEmpty()) {
 							Command undoCommand = layerBitmapCommand.getLayerUndoCommands().get(0);
 							if (undoCommand instanceof ResizeCommand) {
-								TransformTool.undoResizeCommand(layer, (ResizeCommand) undoCommand);
+								undoResizeCommand(layer, (ResizeCommand) undoCommand);
 							} else if (undoCommand instanceof RotateCommand) {
-								TransformTool.undoRotateCommand(layer, (RotateCommand) undoCommand);
+								undoRotateCommand(layer, (RotateCommand) undoCommand);
 							}
 						}
 					}
@@ -222,9 +358,9 @@ public final class UndoRedoManager {
 
 					// check for resize/rotate
 					if (lastUndoCommand instanceof ResizeCommand) {
-						TransformTool.redoResizeCommand(layer, (ResizeCommand) lastUndoCommand);
+						redoResizeCommand(layer, (ResizeCommand) lastUndoCommand);
 					} else if (lastUndoCommand instanceof RotateCommand) {
-						TransformTool.redoRotateCommand(layer, (RotateCommand) lastUndoCommand);
+						redoRotateCommand(layer, (RotateCommand) lastUndoCommand);
 					}
 				}
 				return null;
