58634bd16cb92b846dc7913bfbf57a5ba0031bfc
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2019-05-10 12:59:49 +0200

    RELEASE-2.4.0: Update language strings

--- Paintroid/src/main/java/org/catrobat/paintroid/tools/ToolType.java
+++ Paintroid/src/main/java/org/catrobat/paintroid/tools/ToolType.java
@@ -45,7 +45,7 @@ public enum ToolType {
 	SHAPE(R.string.button_shape, R.string.help_content_shape, EnumSet.of(StateChange.ALL), R.id.pocketpaint_tools_rectangle, R.drawable.pocketpaint_rectangle_tool_overlay, true),
 	TEXT(R.string.button_text, R.string.help_content_text, EnumSet.of(StateChange.ALL), R.id.pocketpaint_tools_text, R.drawable.pocketpaint_text_tool_overlay, true),
 	LAYER(R.string.layers_title, R.string.help_content_layer, EnumSet.of(StateChange.ALL), R.id.pocketpaint_btn_top_layers, INVALID_RESOURCE_ID, false),
-	COLORCHOOSER(R.string.color_chooser_title, R.string.help_content_color_chooser, EnumSet.of(StateChange.ALL), R.id.pocketpaint_btn_top_color, INVALID_RESOURCE_ID, false),
+	COLORCHOOSER(R.string.color_picker_title, R.string.help_content_color_chooser, EnumSet.of(StateChange.ALL), R.id.pocketpaint_btn_top_color, INVALID_RESOURCE_ID, false),
 	HAND(R.string.button_hand, R.string.help_content_hand, EnumSet.of(StateChange.ALL), R.id.pocketpaint_tools_hand, INVALID_RESOURCE_ID, false);
 
 	private int nameResource;
