tintyapp/Tinty

PAINTROID-87 Current Tool shown in Toolbar

Paintroid/src/main/java/org/catrobat/paintroid/presenter/MainActivityPresenter.java
@@ -379,6 +379,7 @@ public class MainActivityPresenter implements Presenter, SaveImageCallback, Load
 		bottomNavigationViewHolder.setColorButtonColor(toolController.getToolColor());
 		actionBar.initialize();
 		refreshActionBarButtons();
+		bottomNavigationViewHolder.setCurrentTool(toolController.getToolType());
 
 		if (model.isFullscreen()) {
 			enterFullscreen();
@@ -459,6 +460,7 @@ public class MainActivityPresenter implements Presenter, SaveImageCallback, Load
 
 	private void setTool(ToolType toolType) {
 		bottomBarViewHolder.hide();
+		bottomNavigationViewHolder.setCurrentTool(toolType);
 
 		int offset = actionBar.getHeight();
 		navigator.showToolChangeToast(offset, toolType.getNameResource());

Catrobat/Paintroid

Paintroid/src/main/java/org/catrobat/paintroid/presenter/MainActivityPresenter.java
@@ -412,6 +412,7 @@ public class MainActivityPresenter implements Presenter, SaveImageCallback, Load
 	public void finishInitialize() {
 		refreshTopBarButtons();
 		topBarViewHolder.setColorButtonColor(toolController.getToolColor());
+		bottomNavigationViewHolder.showCurrentTool(toolController.getToolType());
 
 		if (model.isFullscreen()) {
 			enterFullscreen();
@@ -516,6 +517,7 @@ public class MainActivityPresenter implements Presenter, SaveImageCallback, Load
 
 	private void setTool(ToolType toolType) {
 		bottomBarViewHolder.hide();
+		bottomNavigationViewHolder.showCurrentTool(toolType);
 
 		int offset = topBarViewHolder.getHeight();
 		navigator.showToolChangeToast(offset, toolType.getNameResource());