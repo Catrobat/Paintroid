88fa0003b491b1c221611e0adb73afd1c71ede4a
Christoph Binder <christoph.binder@student.tugraz.at>
2020-05-20 15:42:13 +0200

    hand/brush

--- Paintroid/src/androidTest/java/org/catrobat/paintroid/test/espresso/util/UiMatcher.java
+++ Paintroid/src/androidTest/java/org/catrobat/paintroid/test/espresso/util/UiMatcher.java
@@ -436,11 +436,7 @@ public final class UiMatcher {
 					Bitmap targetBitmap = ((BitmapDrawable) targetDrawable).getBitmap();
 					expectedBitmap = ((BitmapDrawable) expectedDrawable).getBitmap();
 					return targetBitmap.sameAs(expectedBitmap);
-				} /*else if (targetDrawable instanceof StateListDrawable) {
-					Bitmap targetBitmap = ((BitmapDrawable) targetDrawable.getCurrent()).getBitmap();
-					expectedBitmap = ((BitmapDrawable) expectedDrawable).getBitmap();
-					return targetBitmap.sameAs(expectedBitmap);
-				}*/ else if (targetDrawable instanceof VectorDrawable) {
+				} else if (targetDrawable instanceof VectorDrawable) {
 					Bitmap targetBitmap = vectorToBitmap((VectorDrawable) expectedDrawable);
 					expectedBitmap = vectorToBitmap((VectorDrawable) expectedDrawable);
 					return targetBitmap.sameAs(expectedBitmap);
