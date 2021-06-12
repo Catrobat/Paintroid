f6e6f44bb5125e708ffbc55f3df9716bf0b9928f
Saarthak Seth <saarthakseth@gmail.com>
2021-06-12 16:29:06 +0530

    PAINTROID-262 refactored BrushTool and EraserTool to Kotlin

--- Paintroid/src/main/java/org/catrobat/paintroid/tools/options/BrushToolOptionsView.kt
+++ Paintroid/src/main/java/org/catrobat/paintroid/tools/options/BrushToolOptionsView.kt
@@ -1,6 +1,6 @@
 /*
  * Paintroid: An image manipulation application for Android.
- * Copyright (C) 2010-2015 The Catrobat Team
+ * Copyright (C) 2010-2021 The Catrobat Team
  * (<http://developer.catrobat.org/credits>)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,11 +16,27 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package org.catrobat.paintroid.tools.options
 
-package org.catrobat.paintroid.tools.options;
+import android.graphics.Paint
+import android.graphics.Paint.Cap
+import org.catrobat.paintroid.tools.ToolType
 
-public interface BrushToolPreview {
-	void setListener(BrushToolOptionsView.OnBrushPreviewListener callback);
+interface BrushToolOptionsView {
+    fun invalidate()
+    fun setCurrentPaint(paint: Paint)
+    fun setBrushChangedListener(onBrushChangedListener: OnBrushChangedListener)
+    fun setBrushPreviewListener(onBrushPreviewListener: OnBrushPreviewListener)
 
-	void invalidate();
+    interface OnBrushChangedListener {
+        fun setCap(strokeCap: Cap)
+        fun setStrokeWidth(strokeWidth: Int)
+    }
+
+    interface OnBrushPreviewListener {
+        val strokeWidth: Float
+        val strokeCap: Cap
+        val color: Int
+        val toolType: ToolType
+    }
 }
