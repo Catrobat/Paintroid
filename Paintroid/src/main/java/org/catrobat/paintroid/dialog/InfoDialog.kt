0d20427d8b7b8bf6f528cda193099d3ae507ccf9
Saarthak Seth <saarthakseth@gmail.com>
2021-07-13 22:26:32 +0530

    PAINTROID-314 refactored MainActivityNavigator to Kotlin

--- Paintroid/src/main/java/org/catrobat/paintroid/dialog/InfoDialog.kt
+++ Paintroid/src/main/java/org/catrobat/paintroid/dialog/InfoDialog.kt
@@ -28,6 +28,10 @@ import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.core.os.bundleOf
 import org.catrobat.paintroid.R
 
+private const val DRAWABLE_RESOURCE_KEY = "drawableResource"
+private const val MESSAGE_RESOURCE_KEY = "messageResource"
+private const val TITLE_RESOURCE_KEY = "titleResource"
+
 class InfoDialog : AppCompatDialogFragment(), DialogInterface.OnClickListener {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(requireContext(), R.style.PocketPaintAlertDialog)
@@ -49,23 +53,16 @@ class InfoDialog : AppCompatDialogFragment(), DialogInterface.OnClickListener {
     }
 
     companion object {
-        const val DRAWABLE_RESOURCE_KEY = "drawableResource"
-        const val MESSAGE_RESOURCE_KEY = "messageResource"
-        const val TITLE_RESOURCE_KEY = "titleResource"
-
-        @JvmStatic
         fun newInstance(
             dialogType: DialogType,
             @StringRes messageResource: Int,
             @StringRes titleResource: Int
-        ): InfoDialog {
-            return InfoDialog().apply {
-                arguments = bundleOf(
-                    DRAWABLE_RESOURCE_KEY to dialogType.imageResource,
-                    MESSAGE_RESOURCE_KEY to messageResource,
-                    TITLE_RESOURCE_KEY to titleResource
-                )
-            }
+        ): InfoDialog = InfoDialog().apply {
+            arguments = bundleOf(
+                DRAWABLE_RESOURCE_KEY to dialogType.imageResource,
+                MESSAGE_RESOURCE_KEY to messageResource,
+                TITLE_RESOURCE_KEY to titleResource
+            )
         }
     }
 }
