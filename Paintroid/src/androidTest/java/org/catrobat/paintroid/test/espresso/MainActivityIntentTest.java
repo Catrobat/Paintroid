ede3abcfcea7219071023e57736eb1a8b6d52d1b
SebastianGrief <sebastian.grief@student.tugraz.at>
2022-07-11 18:14:49 +0200

    PAINTROID-356 static analysis warnings

--- Paintroid/src/androidTest/java/org/catrobat/paintroid/test/espresso/MainActivityIntentTest.java
+++ Paintroid/src/androidTest/java/org/catrobat/paintroid/test/espresso/MainActivityIntentTest.java
@@ -1,5 +1,11 @@
 package org.catrobat.paintroid.test.espresso;
 
+import static org.catrobat.paintroid.test.espresso.util.UiInteractions.touchAt;
+import static org.catrobat.paintroid.test.espresso.util.wrappers.DrawingSurfaceInteraction.onDrawingSurfaceView;
+import static org.catrobat.paintroid.test.espresso.util.wrappers.ToolBarViewInteraction.onToolBarView;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -26,12 +32,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 
-import static org.catrobat.paintroid.test.espresso.util.UiInteractions.touchAt;
-import static org.catrobat.paintroid.test.espresso.util.wrappers.DrawingSurfaceInteraction.onDrawingSurfaceView;
-import static org.catrobat.paintroid.test.espresso.util.wrappers.ToolBarViewInteraction.onToolBarView;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -41,82 +41,82 @@ import java.util.Objects;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityIntentTest {
 
-    @Rule
-    public ActivityTestRule<MainActivity> launchActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+	@Rule
+	public ActivityTestRule<MainActivity> launchActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    private ContentResolver contentResolver;
-    private static ArrayList<File> deletionFileList = null;
+	private ContentResolver contentResolver;
+	private static ArrayList<File> deletionFileList = null;
 
-    @Before
-    public void setUp() {
-        deletionFileList = new ArrayList<>();
-        contentResolver = launchActivityTestRule.getActivity().getContentResolver();
-    }
+	@Before
+	public void setUp() {
+		deletionFileList = new ArrayList<>();
+		contentResolver = launchActivityTestRule.getActivity().getContentResolver();
+	}
 
-    @After
-    public void tearDown() {
-        for (File file : deletionFileList) {
-            if (file != null && file.exists()) {
-                assertTrue(file.delete());
-            }
-        }
-    }
+	@After
+	public void tearDown() {
+		for (File file : deletionFileList) {
+			if (file != null && file.exists()) {
+				assertTrue(file.delete());
+			}
+		}
+	}
 
-    @Test
-    public void testAppliedChangesAfterOrientationChangePersist() {
-        MockitoAnnotations.initMocks(this);
-        Intents.init();
+	@Test
+	public void testAppliedChangesAfterOrientationChangePersist() {
+		MockitoAnnotations.initMocks(this);
+		Intents.init();
 
-        Uri testUri = createTestImageFile();
+		Uri testUri = createTestImageFile();
 
-        Intent intent = new Intent(Intent.ACTION_SEND)
-                .setType("image/*")
-                .putExtra(Intent.EXTRA_STREAM, testUri);
+		Intent intent = new Intent(Intent.ACTION_SEND)
+				.setType("image/*")
+				.putExtra(Intent.EXTRA_STREAM, testUri);
 
-        launchActivityTestRule.launchActivity(intent);
+		launchActivityTestRule.launchActivity(intent);
 
-        assertNull(launchActivityTestRule.getActivity().model.getSavedPictureUri());
-        assertNull(launchActivityTestRule.getActivity().model.getCameraImageUri());
+		assertNull(launchActivityTestRule.getActivity().model.getSavedPictureUri());
+		assertNull(launchActivityTestRule.getActivity().model.getCameraImageUri());
 
-        onDrawingSurfaceView()
-                .checkPixelColor(Color.TRANSPARENT, BitmapLocationProvider.MIDDLE);
+		onDrawingSurfaceView()
+				.checkPixelColor(Color.TRANSPARENT, BitmapLocationProvider.MIDDLE);
 
-        onToolBarView().performSelectTool(ToolType.FILL);
+		onToolBarView().performSelectTool(ToolType.FILL);
 
-        onDrawingSurfaceView()
-                .perform(touchAt(DrawingSurfaceLocationProvider.MIDDLE));
+		onDrawingSurfaceView()
+				.perform(touchAt(DrawingSurfaceLocationProvider.MIDDLE));
 
-        onDrawingSurfaceView()
-                .checkPixelColor(Color.BLACK, BitmapLocationProvider.MIDDLE);
+		onDrawingSurfaceView()
+				.checkPixelColor(Color.BLACK, BitmapLocationProvider.MIDDLE);
 
-        launchActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		launchActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        onDrawingSurfaceView()
-                .checkPixelColor(Color.BLACK, BitmapLocationProvider.MIDDLE);
-    }
+		onDrawingSurfaceView()
+				.checkPixelColor(Color.BLACK, BitmapLocationProvider.MIDDLE);
+	}
 
-    private Uri createTestImageFile() {
-        Bitmap bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888);
+	private Uri createTestImageFile() {
+		Bitmap bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888);
 
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, "testfile.jpeg");
-        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
-        }
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, "testfile.jpeg");
+		contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+			contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
+		}
 
-        Uri imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
-        try {
-            OutputStream fos = contentResolver.openOutputStream(Objects.requireNonNull(imageUri));
-            assertTrue(bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos));
-            assert fos != null;
-            fos.close();
-        } catch (IOException e) {
-            throw new AssertionError("Picture file could not be created.", e);
-        }
+		Uri imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+		try {
+			OutputStream fos = contentResolver.openOutputStream(Objects.requireNonNull(imageUri));
+			assertTrue(bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos));
+			assert fos != null;
+			fos.close();
+		} catch (IOException e) {
+			throw new AssertionError("Picture file could not be created.", e);
+		}
 
-        File imageFile = new File(imageUri.getPath(), "testfile.jpeg");
-        deletionFileList.add(imageFile);
-        return imageUri;
-    }
+		File imageFile = new File(imageUri.getPath(), "testfile.jpeg");
+		deletionFileList.add(imageFile);
+		return imageUri;
+	}
 }
