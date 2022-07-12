cb4bd6cf1581e1fa3b5eb6c1918e933ddeba80aa
SebastianGrief <sebastian.grief@student.tugraz.at>
2022-07-12 17:21:29 +0200

    PAINTROID-356 added Intent release

--- Paintroid/src/androidTest/java/org/catrobat/paintroid/test/espresso/MainActivityIntentTest.java
+++ Paintroid/src/androidTest/java/org/catrobat/paintroid/test/espresso/MainActivityIntentTest.java
@@ -92,6 +92,8 @@ public class MainActivityIntentTest {
 
 		onDrawingSurfaceView()
 				.checkPixelColor(Color.BLACK, BitmapLocationProvider.MIDDLE);
+
+		Intents.release();
 	}
 
 	private Uri createTestImageFile() {
