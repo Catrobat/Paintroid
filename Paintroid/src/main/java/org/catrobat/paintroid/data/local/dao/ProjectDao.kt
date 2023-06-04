fd4a97a50a039b62c0bb3d8fea11e9115aacdc08
Rd <rd4ramadevi@gmail.com>
2023-06-04 09:04:35 +0530

    Room Database Declaration

---
+++ Paintroid/src/main/java/org/catrobat/paintroid/data/local/dao/ProjectDao.kt
@@ -0,0 +1,25 @@
+package org.catrobat.paintroid.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import org.catrobat.paintroid.model.Project
+
+@Dao
+interface ProjectDao {
+
+    @Insert
+    fun insertProject(project: Project)
+
+    @Query("UPDATE Project SET path= :projectUri, imagePreviewPath= :imagePreviewPath WHERE name= :name")
+    fun updateProjectUri(name: String, imagePreviewPath: String, projectUri: String)
+
+    @Query("SELECT * FROM Project ORDER BY lastModified DESC")
+    fun getProjects(): List<Project>
+
+    @Query("DELETE FROM Project WHERE id= :id")
+    fun deleteProject(id: Int)
+
+    @Query("DELETE FROM Project")
+    fun deleteAllProjects()
+}
