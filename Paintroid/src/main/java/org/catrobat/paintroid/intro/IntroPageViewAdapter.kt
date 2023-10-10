784626c738806b28bf4b48a2bc67851cb0f23305
Rd <rd4ramadevi@gmail.com>
2023-10-10 15:04:48 +0530

    PAINTROID-534: Refactored introPageViewAdapter to kotlin

---
+++ Paintroid/src/main/java/org/catrobat/paintroid/intro/IntroPageViewAdapter.kt
@@ -0,0 +1,45 @@
+/**
+ * Paintroid: An image manipulation application for Android.
+ * Copyright (C) 2010-2015 The Catrobat Team
+ * (<http:></http:>//developer.catrobat.org/credits>)
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http:></http:>//www.gnu.org/licenses/>.
+ */
+package org.catrobat.paintroid.intro
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+
+class IntroPageViewAdapter(private val layouts: IntArray) : PagerAdapter() {
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val inflater = LayoutInflater.from(container.context)
+        val view = inflater.inflate(layouts[position], container, false)
+        container.addView(view)
+        return view
+    }
+
+    override fun getCount(): Int = layouts.size
+
+    override fun isViewFromObject(view: View, obj: Any): Boolean = view === obj
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+}
