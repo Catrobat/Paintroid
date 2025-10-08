bf01f42f9b3f1d7db0c95865e900a738db4ca3c1
Florian Kanduth <florian.kanduth@student.tugraz.az>
2025-10-08 17:25:16 +0200

    remove unnecessary packages from container

--- buildScripts/startEmulator.sh
+++ buildScripts/startEmulator.sh
@@ -8,6 +8,7 @@ adb start-server
 echo no | avdmanager create avd --force --name "android${android_version}" --package "system-images;android-${android_version};default;x86_64"
 emulator -no-window -no-boot-anim -noaudio -avd "android${android_version}" > "${stageName}_emulator.log" 2>&1 &
 
+echo "Wait for device..."
 adb devices
 timeout 5m adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1;done'
 echo "Emulator started"
